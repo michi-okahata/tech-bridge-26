af01555dd112966e6d86e88cc1c4fdde76bc7000
michi-okahata <michi.okahata@outlook.com>
2025-03-20 18:22:32 -0400

    process one shot

---
+++ process.py
@@ -0,0 +1,28 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('data/choice.db')
+cursor = conn.cursor()
+
+rename = """
+ALTER TABLE rw_org
+RENAME COLUMN '[' TO org_id;
+"""
+
+cursor.execute(rename)
+
+prediction = """
+CREATE TABLE prediction AS
+SELECT header.donationdate posting_date,
+header.donorname donor_id,
+lines.totalgrossweight gross_weight,
+header.warehousename branch_code,
+lines.storagerequirement storage_code
+FROM amx_donation_header header
+JOIN amx_donation_lines lines ON header.donationnumber = lines.donationnumber
+"""
+
+cursor.execute(prediction)
+
+conn.commit()
+conn.close()
